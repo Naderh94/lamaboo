3c46ea5185a24d300316136ccfbb90a311155c22
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-02-20 17:12:07 -0700

    Added Top images and scroll bar css

---
+++ pages/components/TopImages.jsx
@@ -0,0 +1,161 @@
+import React, { useRef, useState, useEffect } from "react";
+
+const TopImages = () => {
+  const containerRef = useRef(null);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleWheel = (event) => {
+    if (isHovering) {
+      event.preventDefault();
+      const { current } = containerRef;
+      current.scrollLeft += event.deltaY;
+    }
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+    document.body.style.overflow = "auto";
+  };
+
+  useEffect(() => {
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        overflowX: "auto",
+        display: "flex",
+        flexDirection: "row",
+        whiteSpace: "nowrap",
+        width: "100%",
+      }}
+      onWheel={handleWheel}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="outer-wrapperimg">
+        <div
+          className="wrapperimg snaps-inline"
+          style={{ borderRadius: "3px", padding: "10px", whiteSpace: "nowrap" }}
+        >
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+          <div className="topimg">
+          <picture>
+              <img alt="..." loading="lazy" src="https://via.placeholder.com/200x200" />
+            </picture>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopImages;
