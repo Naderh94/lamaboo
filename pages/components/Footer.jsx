e7e0be07a66b9aa4c01483099fd7f11767b7762b
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-02-24 19:32:13 -0700

    footer emojis

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <footer class="footer">
-      <p> ❤️ <span style={{ color: '#444343' }}> Created with all the </span> <span style={{ color: '#ff0000' }}>LOVE</span> <span style={{ color: '#444343' }}> in the </span> 🌎  <span style={{ color: '#444343' }}> by </span> <span style={{ color: '#57d6b6' }}>Nadoura el Amoura</span> ❤️</p>
+      <p> ❤️ <span style={{ color: '#444343' }}> Created with all the </span> <span style={{ color: '#ff0000' }}>LOVE💕</span> <span style={{ color: '#444343' }}> in the </span> 🌎  <span style={{ color: '#444343' }}> by </span> <span style={{ color: '#57d6b6' }}>Nadoura el Amoura</span> 🤟🏼</p>
     </footer>
   );
 }
