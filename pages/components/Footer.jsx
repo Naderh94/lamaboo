0ba33ca701b4d999cdf890f8373411f41c6a82fc
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-02-24 19:48:15 -0700

    footer and slide text size

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <footer class="footer">
-      <p> 💐 <span style={{ color: '#444343' }}> Created with all the </span> <span style={{ color: '#ff0000' }}>❤️ LOVE ❤️</span> <span style={{ color: '#444343' }}> in the </span> 🌎  <span style={{ color: '#444343' }}> by </span> <span style={{ color: '#57d6b6' }}>Nadoura el Amoura</span> 🤟🏼</p>
+      <p> 💐 <span style={{ color: '#444343' }}> Created with all the </span> <span style={{ color: '#ff0000' }}>❤️ LOVE ❤️</span> <span style={{ color: '#444343' }}> in the </span> 🌎  <span style={{ color: '#444343' }}> by: </span> <span style={{ color: '#57d6b6' }}>Nadoura el Amoura</span> 🤟🏼</p>
     </footer>
   );
 }
