057fe6dc5cd303e59503d249daeb9c513820f79b
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-02-26 20:41:27 -0700

    style changes to password

--- pages/components/PasswordScreen.tsx
+++ pages/components/PasswordScreen.tsx
@@ -17,16 +17,84 @@ export default function PasswordScreen({ onPasswordEntered }: Props) {
   }
 
   return (
-    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: '#eedac9', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div style={{ padding: '1.5rem', backgroundColor: '#db5353', borderRadius: '0.3rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <form onSubmit={handleFormSubmit} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', maxWidth: '100%' }}>
-          <label style={{ color: 'black', display: 'block', fontSize: '1.3rem', fontWeight: "bold", fontFamily: 'Roboto Slab', paddingRight: '1.3rem' }}>
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#eedac9',
+        padding: '2rem'
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: '#db5353',
+          borderRadius: '0.5rem',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '2rem',
+          maxWidth: '90vw'
+        }}
+      >
+        <form onSubmit={handleFormSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <label
+            style={{
+              color: 'black',
+              fontSize: '1.3rem',
+              fontWeight: 'bold',
+              fontFamily: 'Roboto Slab',
+              marginBottom: '1.5rem',
+              textAlign: 'center'
+            }}
+          >
             Please enter the password to view my favorite thing in life:
-            <input type="text" value={password} onChange={handlePasswordChange} style={{ padding: '0.01rem', borderRadius: '0.9rem', marginLeft: '1.3rem', fontFamily: 'Roboto Slab', textAlign: 'center', fontWeight: 'lighter' }} />
+            <input
+              type="text"
+              value={password}
+              onChange={handlePasswordChange}
+              style={{
+                padding: '0.5rem',
+                borderRadius: '0.5rem',
+                marginLeft: '1.3rem',
+                fontFamily: 'Roboto Slab',
+                textAlign: 'center',
+                fontWeight: 'lighter',
+                border: 'none',
+                borderBottom: '0.15rem solid #000000',
+                outline: 'none',
+                backgroundColor: 'transparent',
+                color: '#000000',
+                width: '100%',
+                marginTop: '0.5rem'
+              }}
+            />
           </label>
-          <div style={{ fontSize: '1.5rem' }}>
-            <button type="submit" className="submitbutton bg-gray-800 hover:bg-gray-900 font-bold rounded focus:outline-none focus:shadow-outline">Yes Daddy</button>
-          </div>
+          <button
+            type="submit"
+            className="submitbutton"
+            style={{
+              backgroundColor: '#000000',
+              color: '#db5353',
+              borderRadius: '0.5rem',
+              padding: '0.5rem 1rem',
+              fontFamily: 'Roboto Slab',
+              fontWeight: 'bold',
+              fontSize: '1.2rem',
+              outline: 'none',
+              border: 'none',
+              cursor: 'pointer',
+              marginTop: '1rem'
+            }}
+          >
+            Yes Daddy
+          </button>
         </form>
       </div>
     </div>
