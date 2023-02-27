68c4ea33f73a7472c8daa4ce93bb72ac20bbbecb
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-02-26 17:36:00 -0700

    layover password

--- pages/components/PasswordScreen.tsx
+++ pages/components/PasswordScreen.tsx
@@ -1,30 +1,32 @@
 import { useState } from 'react'
 
-type Props = {
+interface Props {
   onPasswordEntered: (password: string) => void
 }
 
-const PasswordScreen = ({ onPasswordEntered }: Props) => {
+export default function PasswordScreen({ onPasswordEntered }: Props) {
   const [password, setPassword] = useState('')
 
-  const handleSubmit = (event) => {
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value)
+  }
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    if (password === 'lamaishot') {
-      onPasswordEntered(password)
-    }
+    onPasswordEntered(password)
   }
 
   return (
-    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
-        <label className='Password'>
-          Password:
-          <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-        </label>
-        <button type="submit" className='Password'>Submit</button>
-      </form>
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ padding: '20px', backgroundColor: '#f1f1f1', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)' }}>
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            Please enter the password to view this site:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
-
-export default PasswordScreen
