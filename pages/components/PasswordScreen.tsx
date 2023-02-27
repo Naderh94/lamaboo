2d0051969775215e566a2af90aae57d3a6e7f6d7
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-02-26 20:07:31 -0700

    font password

--- pages/components/PasswordScreen.tsx
+++ pages/components/PasswordScreen.tsx
@@ -17,14 +17,17 @@ export default function PasswordScreen({ onPasswordEntered }: Props) {
   }
 
   return (
-    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: '#EEEEEE', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div style={{ padding: '20px', backgroundColor: '#F2921D', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: '#eedac9', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ padding: '1.5rem', backgroundColor: '#db5353', borderRadius: '0.3rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
         <form onSubmit={handleFormSubmit} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-          <label style={{ color: '#2E3840', paddingRight: '2.5vw', display: 'block', fontSize: '1.8vh', fontWeight: "bold", fontFamily: 'Roboto Slab' }}>
-            Please enter the password to view this site:
-            <input type="text" value={password} onChange={handlePasswordChange} style={{ padding: '5px', width: '14vw', height: '2.5vh', borderRadius: '8px', marginLeft: '2vw', fontWeight: 'initial', fontFamily: 'monospace', textAlign: 'center' }} />
+          <label style={{ color: 'black', display: 'block', fontSize: '1.3rem', fontWeight: "bold", fontFamily: 'Roboto Slab', paddingRight: '1.3rem' }}>
+            Please enter the password to view my favorite thing in life:
+            <input type="text" value={password} onChange={handlePasswordChange} style={{ padding: '0.01rem', borderRadius: '0.9rem', marginLeft: '1.3rem', fontFamily: 'Roboto Slab', textAlign: 'center', fontWeight: 'lighter' }} />
           </label>
-          <button type="submit" className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline submitbutton">Yes Daddy</button>
+          <div style={{ fontSize: '1.5rem' }}>
+
+          <button type="submit" className="bg-gray-800 hover:bg-gray-900 font-bold rounded focus:outline-none focus:shadow-outline submitbutton">Yes Daddy</button>
+          </div>
         </form>
       </div>
     </div>
