c0b4d7208946c8d0955344443a6ebb51f9d8199d
Naderh94 <123039232+Naderh94@users.noreply.github.com>
2023-03-02 01:52:31 -0700

    seeing if apple fonts change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,700&family=Raleway:ital,wght@1,300&family=Roboto+Slab&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,700&family=Raleway:ital,wght@1,300&family=Roboto+Mono&family=Roboto+Slab&family=Shantell+Sans:wght@300&family=Tilt+Neon&family=Tilt+Warp&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
